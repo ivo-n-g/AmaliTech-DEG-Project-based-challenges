49f8e0a962ee68623e00feb2a9a57c0a2db1a809
ivo-n-g <ganzaivo1@gmail.com>
2026-04-24 13:14:31 +0200

    Refactor payment processing to use Pydantic model for request validation.

--- backend/Idempotency-gateway/main.py
+++ backend/Idempotency-gateway/main.py
@@ -1,52 +1,63 @@
 from fastapi import FastAPI, Header, HTTPException, Response
+from pydantic import BaseModel
 import asyncio
 import time
 from Storage import get_transaction, save_transaction
 
 app = FastAPI(title="FinSafe Idempotency Gateway")
-
-# Async lock to prevent race conditions 
 request_lock = asyncio.Lock()
+
+#Define exactly what the data should look like
+class PaymentData(BaseModel):
+    amount: int
+    currency: str = "GHS"
+
+    # This tells Swagger UI to pre-fill the white box
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "amount": 100,
+                "currency": "GHS"
+            }
+        }
+    }
 
 @app.post("/process-payment")
 async def process_payment(
-    body: dict,  
+    body: PaymentData,  # We use our strict model here
     response: Response,
     idempotency_key: str = Header(None)
 ):
-    # Validate header presence
     if not idempotency_key:
         raise HTTPException(status_code=400, detail="Idempotency-Key header is required")
 
-    # Use lock to handle overlapping identical requests
+    # Convert the strict model back into a standard dictionary for our storage logic
+    payload_dict = body.model_dump()
+
     async with request_lock:
         existing = get_transaction(idempotency_key)
 
         if existing:
-            # Check if this is a fraud/error attempt (different body, same key)
-            if existing["payload"] != body:
+            if existing["payload"] != payload_dict:
                 raise HTTPException(
                     status_code=409,
                     detail="Idempotency key already used for a different request body."
                 )
             
-            # Return cached response for legitimate retries
             response.headers["X-Cache-Hit"] = "true"
             return existing["response"]
 
-        # --- Simulate Bank Processing  ---
+        # --- Simulate Bank Processing ---
         await asyncio.sleep(2) 
 
-        # Generate successful response
         charge_result = {
             "status": "success",
-            "message": f"Charged {body.get('amount')} {body.get('currency', 'GHS')}",
-            "amount": body.get("amount"),
-            "currency": body.get("currency", "GHS"),
+            "message": f"Charged {payload_dict.get('amount')} {payload_dict.get('currency')}",
+            "amount": payload_dict.get("amount"),
+            "currency": payload_dict.get("currency"),
             "timestamp": time.strftime("%Y-%m-%d %H:%M:%S")
         }
 
-        # Persist for idempotency
-        save_transaction(idempotency_key, body, charge_result)
+        save_transaction(idempotency_key, payload_dict, charge_result)
 
         return charge_result
